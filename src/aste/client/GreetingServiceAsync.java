6e0c63b5ad14e4ffbfdb47a70eabf8b2a1b3b1fe
stefi98 <stefania.rizzo6@studio.unibo.it>
2020-11-04 09:59:09 +0100

    sprint 1
    realizzazione delle interfacce previste nel primo sprint.

---
+++ src/aste/client/GreetingServiceAsync.java
@@ -0,0 +1,16 @@
+package aste.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback) throws IllegalArgumentException;
+
+	void login(String input, AsyncCallback<Boolean> asyncCallback);
+
+	void start(AsyncCallback<String> asyncCallback);
+
+	//void signIn(Utente utente, AsyncCallback<String> asyncCallback);
+}
